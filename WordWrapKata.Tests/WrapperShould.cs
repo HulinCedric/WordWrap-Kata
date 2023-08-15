b9d569019f5b21e68d335bbe56ede6934300692e
Cédric Hulin <hulin.cedric@gmail.com>
2023-08-15 19:30:44 +0200

    Build in a row => Text -> Sentence processor
    Not optimized, just try to be explicit in the meaning and try to represent element without over engineer too much

--- WordWrapKata.Tests/WrapperShould.cs
+++ WordWrapKata.Tests/WrapperShould.cs
@@ -6,6 +6,75 @@ namespace WordWrapKata.Tests;
 public class WrapperShould
 {
     [Fact]
-    public void Fixme()
+    public void Return_empty_string_when_text_is_null()
         => Wrapper.Wrap(null, 10).Should().Be("");
+    
+    [Fact]
+    public void Return_empty_string_when_text_is_empty_string()
+        => Wrapper.Wrap("", 10).Should().Be("");
+    
+    [Fact]
+    public void Test_1()
+        => Wrapper.Wrap("aaa", 10).Should().Be("aaa");
+    
+    [Fact]
+    public void Test_2()
+        => Wrapper.Wrap("aaa", 2).Should().Be("aa\na");
+    
+    [Fact]
+    public void Test_3()
+        => Wrapper.Wrap("aabbc", 2).Should().Be("aa\nbb\nc");
+    
+    
+    [Fact]
+    public void Test_4()
+        => Wrapper.Wrap("aa\nbb\nc", 2).Should().Be("aa\nbb\nc");
+    
+    [Fact]
+    public void Test_5()
+        => Wrapper.Wrap("aa\nbb\nc", 10).Should().Be("aa\nbb\nc");
+    
+    
+    [Fact]
+    public void Test_6()
+        => Wrapper.Wrap("aa\n\nbbc\nddeef", 2).Should().Be("aa\n\nbb\nc\ndd\nee\nf");
+    
+    
+    [Fact]
+    public void Return_empty_string_when_wrap_null()
+        => Wrapper.Wrap(null, 10).Should().Be("");
+
+    [Fact]
+    public void Return_empty_string_when_wrap_empty_string()
+        => Wrapper.Wrap("", 10).Should().Be("");
+
+    [Fact]
+    public void Not_wrap_one_short_word()
+        => Wrapper.Wrap("word", 5).Should().Be("word");
+
+    [Fact]
+    public void Breaks_at_length_when_word_longer_than_length()
+    {
+        Wrapper.Wrap("longword", 4).Should().Be("long\nword");
+        Wrapper.Wrap("longerword", 6).Should().Be("longer\nword");
+    }
+
+    [Fact]
+    public void Break_twice_when_word_longer_than_twice_length()
+        => Wrapper.Wrap("verylongword", 4).Should().Be("very\nlong\nword");
+
+    [Fact]
+    public void Wrap_when_two_words_longer_than_limit()
+    {
+        Wrapper.Wrap("word word", 6).Should().Be("word\nword");
+        Wrapper.Wrap("wrap me", 6).Should().Be("wrap\nme");
+    }
+
+    [Fact]
+    public void Wrap_when_three_words_each_longer_than_limit()
+        => Wrapper.Wrap("word word word", 4).Should().Be("word\nword\nword");
+
+    [Fact]
+    public void Break_at_second_word_when_three_words_just_over_the_Limit()
+        => Wrapper.Wrap("word word word", 11).Should().Be("word word\nword");
 }
--- WordWrapKata/Wrapper.cs
+++ WordWrapKata/Wrapper.cs
@@ -1,8 +1,54 @@
 ﻿namespace WordWrapKata;
 
-public class Wrapper
+internal readonly record struct Sentence(string Value)
 {
-    public static string Wrap(string s, int col)
+    public Sentence[] Wrap(int length)
+        => Wrap(Value, length).Split("\n").Select(New).ToArray();
+
+    private static string Wrap(string sentence, int length)
     {
+        if (sentence.Length <= length)
+            return sentence;
+
+        var space = sentence
+            .Substring(0, length + 1)
+            .LastIndexOf(" ", StringComparison.Ordinal);
+
+        if (space >= 0)
+            return BreakBetween(sentence, space, space + 1);
+
+        return BreakBetween(sentence, length, length);
     }
+
+    private static string BreakBetween(string s, int start, int end)
+        => s.Substring(0, start) +
+           "\n" +
+           Wrap(s.Substring(end), start);
+
+    public static Sentence New(string sentence)
+        => new(sentence);
+}
+
+public static class Wrapper
+{
+    public static string Wrap(string? text, int column)
+    {
+        if (text is null)
+            return "";
+
+        return SentencesFrom(text)
+            .BreakSentencesAt(column)
+            .ToText();
+    }
+
+    private static Sentence[] SentencesFrom(string text)
+        => text.Split("\n").Select(Sentence.New).ToArray();
+
+    private static IEnumerable<Sentence> BreakSentencesAt(this IEnumerable<Sentence> sentences, int column)
+        => sentences.SelectMany(sentence => sentence.Wrap(column));
+
+    private static string ToText(this IEnumerable<Sentence> sentences)
+        => sentences
+            .Select(s => s.Value)
+            .Aggregate((wrappedText, sentence) => $"{wrappedText}\n{sentence}");
 }
